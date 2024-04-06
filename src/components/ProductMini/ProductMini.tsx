80e679debb463504f4b2e8966c08589d5ecda219
LaptopNickolas <panteleew.kolya@gmail.com>
2024-04-06 18:04:54 +0900

    Добавил кнопку "добавить в корзину"

--- src/components/ProductMini/ProductMini.tsx
+++ src/components/ProductMini/ProductMini.tsx
@@ -4,8 +4,9 @@ import './ProductMini.css';
 
 export const ProductMini: FC<ProductType> = ({ description,image,price,rating,title,id,amount}) => {
 	return <li className='product-mini'>
-		<p className='product-mini__title'>{title}</p>
 		<img className='product-mini__image' src={image} alt={title}/>
+		<p className='product-mini__title'>{title}</p>
 		<p className='product-mini__price'>Цена: {price * 90} руб. Рейтинг: {rating.rate}</p>
+		<button className='product-mini__add-button'>Добавить в корзину</button>
 	</li>;
 };
